d9be9a74412e5a8acdd925849acfbf310d8424d2
Amelia Llewellyn <allewellyn541@gmail.com>
2025-11-21 23:44:49 -0500

    Added html css and js files

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById("searchBtn").addEventListener("click", function () {
+    
+    fetch("superheroes.php")
+        .then(response => response.text()) 
+        .then(data => {
+            alert(data);  
+        })
+        .catch(error => {
+            alert("Error: " + error);
+        });
+
+});
